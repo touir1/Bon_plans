df84e140c3e69d68023f64e1320e9e46f8f01f36
touir1 <touir.mat@gmail.com>
2018-04-15 18:57:11 +0100

    added /build condition

--- Bon_plans_desktop_app/src/tn/esprit/bonplans/Gui/GererAnnonce/administration/validerPlan/ValiderPlanController.java
+++ Bon_plans_desktop_app/src/tn/esprit/bonplans/Gui/GererAnnonce/administration/validerPlan/ValiderPlanController.java
@@ -7,14 +7,20 @@ package tn.esprit.bonplans.Gui.GererAnnonce.administration.validerPlan;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author touir
  */
-public class ValiderPlanController implements Initializable {
+public class ValiderPlanController extends Application implements Initializable {
 
     /**
      * Initializes the controller class.
@@ -24,6 +30,21 @@ public class ValiderPlanController implements Initializable {
         // TODO
         System.out.println(url);
         System.out.println(rb);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ValiderPlan.fxml"));
+        
+        Scene Scene = new Scene(root);
+        primaryStage.getIcons().add(new Image("http://localhost/bon_plans_api/uploads/Logo.png"));
+        primaryStage.setScene(Scene);
+        primaryStage.show();
+        primaryStage.setTitle("Validation plans");
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
     }
     
 }
--- Bon_plans_desktop_app/src/utils/CurrentSession.java
+++ Bon_plans_desktop_app/src/utils/CurrentSession.java
@@ -5,6 +5,7 @@
  */
 package utils;
 
+import java.util.HashMap;
 import java.util.Map;
 import tn.esprit.bonplans.entity.Utilisateur;
 
@@ -13,13 +14,36 @@ import tn.esprit.bonplans.entity.Utilisateur;
  * @author touir
  */
 public class CurrentSession {
-    private static Map<String, Object> data;
+    private static Map<String, Object> dataMap;
     private static Utilisateur utilisateur;
     
     private CurrentSession(){};
     
     private static boolean initCurrentSession(){
-        
+        if(dataMap == null){
+            dataMap = new HashMap<String, Object>();
+        }
         return true;
+    }
+    
+    public static void setUtilisateur(Utilisateur user){
+        if(initCurrentSession()){
+            utilisateur = user;
+        }
+    }
+    
+    public static <E> void addData(String key, E data){
+        if(initCurrentSession()){
+            dataMap.put(key, data);
+        }
+        
+    }
+    
+    public static <E> Object getData(String key){
+        if(initCurrentSession()){
+            return dataMap.get(key);
+        }
+        
+        return null;
     }
 }
