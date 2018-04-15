e665bd655dc8a37d526723839f2dd3b6add03b7b
touir1 <touir.mat@gmail.com>
2018-04-15 17:50:58 +0100

    testing reset

--- git_all.bat
+++ git_all.bat
@@ -5,6 +5,7 @@ call git add *
 if ERRORLEVEL 1 goto error
 call git reset -- Bon_plans_desktop_app/src/resources/common/properties/database.config.properties
 call git reset -- Bon_plans_desktop_app/src/resources/common/properties/upload.server.config.properties
+call git reset -- www/bon_plans_api/uploads/*
 set /p message="Enter commit message: "
 call git commit -m "%message%"
 if ERRORLEVEL 1 goto error
