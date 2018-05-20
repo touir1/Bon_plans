400d913234e8df568d51f3e778553d6c2e04b59a
kays-chetoui <kays.chetoui@gmail.com>
2018-05-20 22:25:05 +0100

    update template user

---
+++ Bon_plans_desktop_app/src/tn/esprit/bonplans/Gui/GererUtilisateur/client/modifier/ModifierController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.bonplans.Gui.GererUtilisateur.client.modifier;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author KC
+ */
+public class ModifierController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
